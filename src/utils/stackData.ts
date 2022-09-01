795ae25c2f3194a9dafb71463b469a32c21606a6
Rafael Ricciardi <ricciardi.rafael1997@gmail.com>
2022-09-01 18:39:16 -0300

    feat: add infos

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,39 +1,37 @@
-import imgHTML from "../public/static/img/stack/html.svg";
-import imgCSS from "../public/static/img/stack/css.svg";
-import imgJS from "../public/static/img/stack/js.svg";
-import imgNode from "../public/static/img/stack/node.svg";
-import imgStyled from "../public/static/img/stack/styled.svg";
-import imgReact from "../public/static/img/stack/react.svg";
-import { FaVuejs } from "react-icons/fa";
-import { SiTypescript } from "react-icons/si";
-import { FaDocker } from "react-icons/fa";
+import imgHTML from '../public/static/img/stack/html.svg';
+import imgCSS from '../public/static/img/stack/css.svg';
+import imgJS from '../public/static/img/stack/js.svg';
+import imgNode from '../public/static/img/stack/node.svg';
+import imgStyled from '../public/static/img/stack/styled.svg';
+import imgReact from '../public/static/img/stack/react.svg';
+import { FaPython } from 'react-icons/fa';
+import { SiTypescript } from 'react-icons/si';
+import { FaDocker } from 'react-icons/fa';
+import { SiExpress } from 'react-icons/si';
+import { SiDjango } from 'react-icons/si';
+import { SiPostgresql } from 'react-icons/si';
 
 export const stackData = [
-  {
-    title: "HTML",
-    img: imgHTML,
-  },
-  {
-    title: "CSS",
-    img: imgCSS,
-  },
-  {
-    title: "JS",
-    img: imgJS,
-  },
-  {
-    title: "Node JS",
-    img: imgNode,
-  },
-  {
-    title: "Styled Components",
-    img: imgStyled,
-  },
-  {
-    title: "React",
-    img: imgReact,
-  },
-  { title: "Vuejs", img: FaVuejs },
-  { title: "TypeScript", img: SiTypescript },
-  { title: "Docker", img: FaDocker },
+	{ title: 'TypeScript', img: SiTypescript },
+	{
+		title: 'Node JS',
+		img: imgNode,
+	},
+	{ title: 'Express', img: SiExpress },
+	{ title: 'Python', img: FaPython },
+	{ title: 'Docker', img: FaDocker },
+	{ title: 'Django', img: SiDjango },
+	{ title: 'PostgreSQL', img: SiPostgresql },
+	{
+		title: 'JS',
+		img: imgJS,
+	},
+	{
+		title: 'Styled Components',
+		img: imgStyled,
+	},
+	{
+		title: 'React',
+		img: imgReact,
+	},
 ];
--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
+	nameUser: 'Rafael Ricciardi',
+	githubUser: 'ricciardi305',
+	linkedinUser: 'rafaelricciardi',
+	whatsappNumber: '+5582987673523',
 };
